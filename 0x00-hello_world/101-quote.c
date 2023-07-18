5c554a094308d324f0e89be4fd0b34eb0189a5bc
Wilfort <juiciwhilf@gmail.com>
2023-07-18 16:28:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Entry Point
  * Return: 1 (Success)
  */
+#include <stdio.h>
+#include <unistd.h>
+
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message);
+       write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+       return (1);
 }

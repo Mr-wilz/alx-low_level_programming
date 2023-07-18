61ebc04c1a0adf63ba7405f0c118cc7e93ac4d14
Wilfort <juiciwhilf@gmail.com>
2023-07-18 16:34:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,9 @@
 #include <unistd.h>
 /**
  * main - Entry Point
+ *
  * Return: 1 (Success)
  */
-#include <stdio.h>
-#include <unistd.h>
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));

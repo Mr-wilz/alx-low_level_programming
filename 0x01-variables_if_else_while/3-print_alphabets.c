862def06163586fb5b2390a4b7a51225f78ef7c8
Wilfort <juiciwhilf@gmail.com>
2023-07-20 09:36:00 -0700

    task 3 aphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowercase = 'a';
+char uppercase = 'A';
+/* Print lowercase alphabet*/
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
+/* Print uppercase alphabet*/
+    while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
+putchar('\n');
+return (0);
+}

901d353a9c19a100dc8e939597514526c1fff4cd
Wilfort <juiciwhilf@gmail.com>
2023-07-20 23:20:16 -0700

    task 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}

7240969073c1ed5a2113d70bdab4c8f38215e68d
Wilfort <juiciwhilf@gmail.com>
2023-07-21 05:01:34 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point of program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+for (int j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(i + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}

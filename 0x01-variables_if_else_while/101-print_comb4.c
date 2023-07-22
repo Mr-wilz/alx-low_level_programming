c8623341cb53a92fdf51abdea827ae3f668bb83f
Wilfort <juiciwhilf@gmail.com>
2023-07-21 19:35:09 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,11 +17,11 @@ for (num3 = num2 + 1; num3 < 10; num3++)
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
 putchar((num3 % 10) + '0');
-}
-if (num1 == 8 && num2 == 9 && num3 == 9)
+if (num1 == 7 && num2 == 8 && num3 == 9)
 continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');

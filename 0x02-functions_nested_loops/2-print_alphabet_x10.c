c791dd69d1eba561ad389aa3bd0e96cc2fbeb691
Wilfort <juiciwhilf@gmail.com>
2023-07-23 00:58:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - program that prints aphabets ten times
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}

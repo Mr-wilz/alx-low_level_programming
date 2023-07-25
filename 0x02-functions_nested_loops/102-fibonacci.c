c47ebd6ef4017c3ec31184125b6a41c9b4c77eb7
Wilfort <juiciwhilf@gmail.com>
2023-07-25 00:22:11 -0700

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,13 @@
 int main(void)
 {
 	int num1 = 1;
-	int num2 = 2;
-	int nextNum;
+	int num2 = 2
 
-	printf("%d, %d", num1, num2)
+	printf("%d, %d", num1, num2);
 
 	for (int i = 2; i < 50; i++)
 	{
-		nextNum = num1 + num2;
+		int nextNum = num1 + num2;
 		printf(", %d", nextNum);
 		num1 = num2;
 		num2 = nextNum;

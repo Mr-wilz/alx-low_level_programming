3e3bd32c2409cc25f36ac27bddd6c7b88f32d1e5
Wilfort <juiciwhilf@gmail.com>
2023-07-27 04:12:48 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,21 @@
 int main(void)
 {
 	int i;
-	unsigned long int j, k, next, sum;
+
+	unsigned long int j, k, next;
 
 	j = 1;
 	k = 2;
-	sum = 0;
 
-	for (i = 1; i <= 33; i++)
+	for (i = 1; i <= 50; i++)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-			sum = sum + j;
+		if (j != 20365011074)
+			printf("%ld, ", j);
+		else
+			printf("%ld\n", j);
 		next = j + k;
 		j = k;
 		k = next;
 	}
-	printf("%lu\n", sum);
-
 	return (0);
 }

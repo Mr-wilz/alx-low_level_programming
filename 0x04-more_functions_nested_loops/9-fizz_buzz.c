93a91778fef9270f673a943203e1e00ad8ed36b7
Wilfort <juiciwhilf@gmail.com>
2023-07-30 14:23:05 -0700

    0x04 task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of program that prints fizzuzz
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+		if (i == 100)
+			printf("\n");
+	}
+	return (0);
+}

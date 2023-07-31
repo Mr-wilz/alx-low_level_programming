4528a1a7ef2f58e8fb96dc3c0d6b3adc5be5ed7c
Wilfort <juiciwhilf@gmail.com>
2023-07-30 18:44:12 -0700

    0x05 task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that points to an int and updates
+ * @n: an integer that inputs pointer
+ * Return: void (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

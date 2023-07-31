f12107e07f700a7ce4f143120a039cbe8a3fc400
Wilfort <juiciwhilf@gmail.com>
2023-07-30 20:03:59 -0700

    0x05 task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: function string to be reversed
+ * Return: void (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char j;
+
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 -i] = j;
+	}
+}

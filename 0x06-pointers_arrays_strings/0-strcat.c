93e08686c6b8b150e5ea68cc04a95d6e1e645285
Wilfort <juiciwhilf@gmail.com>
2023-08-01 06:01:21 -0700

    0x06 task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strcat - function that concatenates two strings
+* @dest: function parameter
+* @src: function parameter
+* Return: dest (success)
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
+}

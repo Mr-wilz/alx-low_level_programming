23dda5926ca4116d8f863f367c66a300ed00dc54
Wilfort <juiciwhilf@gmail.com>
2023-08-04 19:29:12 -0700

    0x0A task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

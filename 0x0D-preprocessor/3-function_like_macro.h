a3d0f5351ac3cea598a8ceeac0986dbb28bb1b98
Wilfort <juiciwhilf@gmail.com>
2023-08-15 18:24:35 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIO_LIKE_MACR0_H
+#define FUNCTIO_LIKE_MACR0_H
+
+/**
+ * file: 3-function_like_macro.h
+ * @x: input
+ * Desc: header file
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x)) 
+
+#endif

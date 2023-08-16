d70b4757d8911538061740f4536edd0b2c660379
Wilfort <juiciwhilf@gmail.com>
2023-08-16 16:36:50 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
 			return (i);

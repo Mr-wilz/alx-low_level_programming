11407eed27e8d925ed20191f05a48f2951fc79b7
Wilfort <juiciwhilf@gmail.com>
2023-08-18 01:10:21 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -33,6 +33,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
+		else if (separator == NULL)
+			return;
 	}
 	printf("\n");
 	va_end(ap);

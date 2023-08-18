d04d72c342388def6256340912f7b9f46b9ec9b9
Wilfort <juiciwhilf@gmail.com>
2023-08-17 17:32:13 -0700

    task 0 sum_them

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - funtion that returns sum of all its parameters
+ * @n: number of args to be passed
+ * @...: 2nd function parameter
+ * Return: 0 (Success)
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+
+	unsgned int m;
+
+	double addNum = (0);
+
+	va_start(ap, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (m = 0; m < n; m++)
+		{
+			addNum += va_arg(ap, int);
+		}
+	}
+
+	va_end(ap);
+
+	return (addNum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Alx project 0x10.c - Variadic functions

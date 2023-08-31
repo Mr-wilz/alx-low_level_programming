8fdca72c9e76621b1215e64b62909376b7dff790
Wilfort <juiciwhilf@gmail.com>
2023-08-31 04:53:36 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t numNode = 0;
+size_t numNode = 0;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		numNode++;
-	}
-	return (numNode);
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+numNode++;
+}
+return (numNode);
 }

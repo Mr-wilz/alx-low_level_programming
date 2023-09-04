821c9cbc512eff86b1e2a290b98d1a89852ea301
Wilfort <juiciwhilf@gmail.com>
2023-09-04 00:03:58 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp = (*head)->next;
 	int data = (*head)->n;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL || &head == NULL)
 		return (0);
 
 	free(*head);

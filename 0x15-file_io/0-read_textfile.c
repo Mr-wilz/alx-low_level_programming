3f50f9859735a18ceb2fc38927aed61cae5f6fdf
Wilfort <juiciwhilf@gmail.com>
2023-09-08 20:00:03 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads text file and  print to STDOUT.
+ * @filename: the file to be read
+ * @letters: number of letters to be read and printed
+ * Return: actual number of bytes (Success) or 0(failure)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	ssize_t b = 0;
+	int c;
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+
+	while (letters > 0 && (c = getc(file)) != EOF)
+	{
+		putchar(c);
+		b++;
+		letters--;
+	}
+
+	fclose(file);
+	return (b);
+}

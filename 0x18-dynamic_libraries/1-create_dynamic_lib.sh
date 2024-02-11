bff61d5075482f4121a40ab3eb054bf24169e18b
Mr-wils <juiciwilf@gmail.com>
2024-02-11 10:08:11 -0800

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-c_files=$(find . -maxdepth 1 -type f -name "*.c"); for file in $c_files; do gcc -c -Wall -Werror -fpic "$file" -o "${file%.c}.o"; done && gcc -shared -o liball.so *.o && rm *.o; echo "Dynamic library liball.so created successfully."
+gcc -c *.c -fpic && gcc *o liball.so && echo "Success."
